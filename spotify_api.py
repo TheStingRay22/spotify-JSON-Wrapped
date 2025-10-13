b3b6b8d8c94cc92b4b8c14ebb771f35d028a9e4c
Dawson Ray <dawson.ray00@gmail.com>
2025-10-13 15:51:03 -0400

    set up test environment and requirtements.txt (closes #4)

--- spotify_api.py
+++ spotify_api.py
@@ -2,6 +2,16 @@
 spotify_api.py
 Future home of API logic
 """
+import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+
+SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+
 
 def placeholder():
     return "Spotify API Integration is on its way!"
